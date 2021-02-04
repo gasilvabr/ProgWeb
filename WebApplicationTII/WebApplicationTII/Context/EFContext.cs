b6f2c38dbded9379782207b86d882a95924e8e0e
George Azevedo da Silva <gasilvabr@hotmail.com>
2021-02-04 11:14:30 -0300

    Capitulo3 Completo

---
+++ WebApplicationTII/WebApplicationTII/Context/EFContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebApplicationTII.Models;
+
+namespace WebApplicationTII.Context
+{
+    public class EFContext: System.Data.Entity.DbContext
+    {
+        public EFContext() : base("Asp_Net_MVC_CS") { }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Fabricante> Fabricantes { get; set; }
+    }
+}
