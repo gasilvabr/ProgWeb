2ae66d2df7dd5d940ce6b0ebb16d0348c0c07f2b
George Azevedo da Silva <gasilvabr@hotmail.com>
2021-06-23 16:33:28 -0300

    Ajuste na tela de fabricante

--- WebApplicationTII/WebApplicationTII/Context/EFContext.cs
+++ WebApplicationTII/WebApplicationTII/Context/EFContext.cs
@@ -9,9 +9,14 @@ namespace WebApplicationTII.Context
 {
     public class EFContext: System.Data.Entity.DbContext
     {
-        public EFContext() : base("Asp_Net_MVC_CS") { }
+        public EFContext() : base("Asp_Net_MVC_CS")
+        {
+            Database.SetInitializer<EFContext>(
+              new DropCreateDatabaseIfModelChanges<EFContext>());
+        }
 
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Fabricante> Fabricantes { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
     }
 }
--- WebApplicationTII/WebApplicationTII/Models/Fabricante.cs
+++ WebApplicationTII/WebApplicationTII/Models/Fabricante.cs
@@ -9,5 +9,7 @@ namespace WebApplicationTII.Models
     {
         public long FabricanteId { get; set; }
         public string Nome { get; set; }
+
+        public virtual ICollection<Produto> Produtos { get; set; }
     }
 }
